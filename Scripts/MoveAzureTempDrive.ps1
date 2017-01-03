f20939365e96567195efd598de4f2b35e258201f
perktime <perktime@users.noreply.github.com>
2017-01-03 11:55:20 -0600

    Add files via upload

---
+++ Scripts/MoveAzureTempDrive.ps1
@@ -0,0 +1,60 @@
+﻿configuration MoveAzureTempDrive
+{
+    param(
+		[Parameter(Mandatory)] 
+        [string]$TempDriveLetter
+    )
+
+    Import-DscResource -ModuleName MoveAzureTempDrive, xComputerManagement
+
+    Node localhost 
+    {
+
+       LocalConfigurationManager 
+       {
+           RebootNodeIfNeeded = $True
+       }      
+               
+        Script DisablePageFile
+        {
+        
+            GetScript  = { @{ Result = "" } }
+            TestScript = { 
+               $pf=gwmi win32_pagefilesetting
+               #There's no page file so okay to enable on the new drive
+               if ($pf -eq $null)
+               {
+                    return $true
+               }
+               #Page file is still on the D drive
+               if ($pf.Name.ToLower().Contains('d:'))
+               {
+                    return $false
+               }
+
+               else
+               {
+                    return $true
+               }
+            
+            }
+            SetScript  = {
+                #Change temp drive and Page file Location 
+                gwmi win32_pagefilesetting
+                $pf=gwmi win32_pagefilesetting
+                $pf.Delete()
+                Restart-Computer -Force
+            }
+           
+        }
+
+	   MoveAzureTempDrive MoveAzureTempDrive
+       {
+		  
+		   TempDriveLetter = $TempDriveLetter           
+       }
+      
+	}
+}
+
+
